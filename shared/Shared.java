b05dd6b1ebf8c2e89075624b2cfbce9087f146db
VT <vtr@gmail.com>
2015-11-24 14:45:56 +0530

    Adds more precise Class Comment for description.

--- shared/Shared.java
+++ shared/Shared.java
@@ -1,7 +1,10 @@
 package in.shared;
 
 /**
-Shared Module for Mobile and Handset.
+*	Shared Module for Tablet and Handset.
+*	Uses:
+*		Creates Shared Object that can be used as Handset/Tablet products.
+*		One product can be created with one object.
 */
 public class Shared{
 	String type;
